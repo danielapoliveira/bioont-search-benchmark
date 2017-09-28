60a3f9e1f563ac571cf2125b4867cac9aa8aed98
danielapoliveira <daniela.oliveira@insight-centre.org>
2017-09-28 22:00:12 +0100

    Added messages.

--- scripts/bulk_load.sh
+++ scripts/bulk_load.sh
@@ -6,6 +6,11 @@ VIRT_DB=$PWD/virt_database
 
 ONTO_DB=$VIRT_DB/tmp_ontologies
 mkdir -p $ONTO_DB
+
+echo
+echo Downloading ontologies...
+echo
+
 wget -N -P $ONTO_DB -i userinput/uris.txt
 
 for i in $ONTO_DB/*.owl;
@@ -16,6 +21,10 @@ do
  echo -e  "$filename" > ${path}${filename}.graph
 done
 
+echo
+echo Bulk loading ontologies, please wait for the it to finish...
+echo
+
 isql $PORT $USER $PASSWORD <<EOF
 DELETE FROM DB.DBA.load_list;
 ld_dir('$ONTO_DB','*.owl', NULL);
@@ -24,3 +33,5 @@ wait_for_children;
 checkpoint;
 EXIT;
 EOF
+
+echo DONE!

b00a2bf0c3c8464e5dbdc82ce2d87e5ef2897860
danielapoliveira <daniela.oliveira@insight-centre.org>
2017-09-28 19:19:50 +0100

    Enforce Python 2

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,11 +1,11 @@
 #|/bin/bash
 
+PYTHON=python2
+
 for i in OLS/ols-apps/ols-solr-app/src/main/resources/*.properties;
 do
- p=$(python scripts/split.py $i) 
+ p=$($PYTHON scripts/split.py $i) 
  echo $p
- #(java -Xmx6g -jar -Dspring.profiles.active=mp OLS/ols-apps/ols-solr-app/target/ols-solr-app.jar && python scripts/appendToFile.py mp false) ||
- #python appendToFile.py mp true
- (java -Xmx6g -jar -Dspring.profiles.active=$p OLS/ols-apps/ols-solr-app/target/ols-solr-app.jar && python scripts/appendToFile.py $p false) ||
- python appendToFile.py $p true
+ (java -Xmx6g -jar -Dspring.profiles.active=$p OLS/ols-apps/ols-solr-app/target/ols-solr-app.jar && $PYTHON scripts/appendToFile.py $p false) ||
+ $PYTHON appendToFile.py $p true
 done

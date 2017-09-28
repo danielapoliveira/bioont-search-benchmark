624cf40f771132f188b53b46c54b39998ad83841
danielapoliveira <daniela.oliveira@insight-centre.org>
2017-09-28 21:36:20 +0100

    Fixed directory paths.

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -2,10 +2,11 @@
 PORT=1151
 USER=dba
 PASSWORD=dba
-VIRT_DB=virt_database
+VIRT_DB=$PWD/virt_database
 
-mkdir $VIRT_DB/tmp_ontologies
-wget -P ontologies -i userinput/uris.txt
+ONTO_DB=VIRT_DB/tmp_ontologies
+mkdir -p $ONTO_DB
+wget -P $ONTO_DB -i userinput/uris.txt
 
 for i in ontologies/*.owl;
 do
@@ -18,7 +19,7 @@ done
 
 isql $PORT $USER $PASSWORD <<EOF
 DELETE FROM DB.DBA.load_list;
-ld_dir('$PWD/ontologies','*.owl', NULL);
+ld_dir('$ONTO_DB','*.owl', NULL);
 rdf_loader_run(log_enable=>3);
 wait_for_children;
 checkpoint;
